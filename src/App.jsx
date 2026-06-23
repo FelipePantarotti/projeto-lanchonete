d6c66cb61aaa0d0ec8ae893c9c69ce9cec1f8008
unknown <felipepantarotti@gmail.com>
2026-06-23 16:31:01 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+import Login from "./components/Login";
+
+function App(){
+  return (
+    <>
+      <Header titulo="Lanchonete Dogão"
+      subtitulo="O melhor da região" />
+      <Login />
+    </>
+  )
+}
+export default App
